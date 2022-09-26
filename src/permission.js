d00fd8e27cae98da7907a745df12db5b79edbd77
Baidada11 <1374673998@qq.com>
2022-09-26 11:46:30 +0800

    feat:头部样式

---
+++ src/permission.js
@@ -0,0 +1,25 @@
+import router from './router'
+import store from './store'
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  // console.log(to)
+  // console.log(from)
+  // next(false)
+  // 1.判断token是否存在
+  // 1.1 存在处于登录状态，是否去往登录页 处于去取首页， 否则放行
+  // 1.2 不存在 不处于登录状态  去往页面  是否处于白名单  处于则放行  否则登录页
+
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
